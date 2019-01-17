24eee5342b104fc602364f6ab4e5423ea5df51e2
KyleHades <46789416+KyleHades@users.noreply.github.com>
2019-01-17 10:19:57 -0600

    Add files via upload

---
+++ CSCE155-C-Lab01/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kyle Paulson
+ * Date: 2019/15/1	
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kyle Paulson\n");
+	printf(Collin);
+  return 0;
+}
